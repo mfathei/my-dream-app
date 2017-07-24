7f351339de06d1f8d8cf742af99770391c288390
Mohammed Mahdy <mohamed.mahdy@sphinxinfotech.net>
2017-07-24 17:56:09 +0200

    lesson 24 register our first component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { RootcaveComponent } from './rootcave/rootcave.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RootcaveComponent
   ],
   imports: [
     BrowserModule,
