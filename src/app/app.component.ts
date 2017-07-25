eebc8a4446b64d73b4ce43d2c7236ae640632f03
Mohammed Mahdy <mohamed.mahdy@sphinxinfotech.net>
2017-07-25 10:40:33 +0200

    lesson 37 ngFor directive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,11 +9,52 @@ export class AppComponent {
   title = "Hello Root Cave";
   imageUrl = "http://lorempixel.com/400/200/sports/1";
 
-  onClick(){
+
+  people = [
+    {
+      "name": "Mahmoud kotoury"
+    },
+    {
+      "name": "Yasmine mohamed"
+    },
+    {
+      "name": "John doe"
+    },
+    {
+      "name": "Jane doe"
+    }
+  ];
+
+  peopleByCountry = [
+    {
+      "country": "EGYPT",
+      "people": [
+        {
+          "name": "Mahmoud"
+        },
+        {
+          "name": "Yasmine"
+        }
+      ]
+    },
+    {
+      "country": "USA",
+      "people": [
+        {
+          "name": "John"
+        },
+        {
+          "name": "Jane"
+        }
+      ]
+    }
+  ];
+
+  onClick() {
     console.log('you clicked button');
   }
 
-  onClick1($event){
-    console.log('you clicked button '+ $event.target.textContent);
+  onClick1($event) {
+    console.log('you clicked button ' + $event.target.textContent);
   }
 }
